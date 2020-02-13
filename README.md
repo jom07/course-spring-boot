3f1e23a1ca95949231377b05a5e885b47a5a9ef4
Jonathan Malatesta <jonathan.malatesta.07@gmail.com>
2020-02-13 13:11:05 -0500

    psql port conflict

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Linkedin course: https://www.linkedin.com/learning/learning-spring-with-spring-boot-2
+
+For the database script in /bin, kill the psql process, then kill all previous dockers then prune docker, then start the script
--- bin/start_postgres.sh
+++ bin/start_postgres.sh
@@ -6,7 +6,7 @@ which psql > /dev/null || (echo "Please ensure that postgres client is in your P
 mkdir -p $HOME/docker/volumes/postgres
 rm -rf $HOME/docker/volumes/postgres/data
 
-docker run --rm --name pg-docker -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=dev -d -p 5433:5433 -v $HOME/docker/volumes/postgres:/var/lib/postgresql postgres
+docker run --rm --name pg-docker -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=dev -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql postgres
 sleep 3
 export PGPASSWORD=postgres
 psql -U postgres -d dev -h localhost -f schema.sql
